5e63746665bef58572d10499ef700810628e5c59
James Scott <jscott@jscott10.com>
2015-08-23 03:31:40 -0400

    fix: File references and modal data

--- js/modal-data.js
+++ js/modal-data.js
@@ -5,22 +5,22 @@
 function setModalData(project_index)	{
 
 	var test_obj = [ {
-						"img_src" : "images/proj0.jpg",
+						"img_src" : "images/proj0-full.jpg",
 						"title" : "Project 0",
 						"subtitle" : "About Me",
 						"link" : "https://github.com/jscott10/UdacityProject0" },
 
 					 {
-					 	"img_src" : "images/proj1.jpg",
+					 	"img_src" : "images/proj1-full.jpg",
 						"title" : "Project 1",
 						"subtitle" : "Portfolio",
 						"link" : "https://github.com/jscott10/UdacityProject1" },
 
 					 {
-					 	"img_src" : "images/proj2.jpg",
+					 	"img_src" : "images/proj2-full.jpg",
 						"title" : "Project 2",
 						"subtitle" : "Interactive Résumé",
-						"link" : "https://github.com/jscott10/UdacityProject2" } ];
+						"link" : "#" } ];
 
 	$('.modal-title', $('#myModal')).append(test_obj[project_index].title);
 	$('.project-image', $('#myModal')).attr("src", test_obj[project_index].img_src);
